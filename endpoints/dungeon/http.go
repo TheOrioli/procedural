d71f33bb0f14e30f5b9d82fe668e2ffdd118409f
Adriano Orioli <orioli.adriano@gmail.com>
2016-08-13 16:21:53 +0200

    dungeon/endpoints: converted from an [][]int grid representation to a graph.

--- endpoints/dungeon/http.go
+++ endpoints/dungeon/http.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/Aorioli/procedural/concerns/direction"
+	"github.com/Aorioli/procedural/concerns/point"
 	"github.com/Aorioli/procedural/endpoints"
 	"github.com/meshiest/go-dungeon/dungeon"
 )
@@ -51,9 +53,14 @@ func decodeRequest(sizeLimit int) func(r *http.Request) (interface{}, error) {
 }
 
 type generateResponse struct {
-	Width  int     `json:"width"`
-	Height int     `json:"height"`
-	Grid   [][]int `json:"dungeon"`
+	Width  int         `json:"width"`
+	Height int         `json:"height"`
+	Grid   []gridPoint `json:"grid"`
+}
+
+type gridPoint struct {
+	P point.Point   `json:"point"`
+	N []point.Point `json:"next"`
 }
 
 func encodeJSONResponse(w http.ResponseWriter, response interface{}) error {
@@ -76,8 +83,58 @@ func encodeJSONResponse(w http.ResponseWriter, response interface{}) error {
 	resp := generateResponse{
 		Width:  v.Size,
 		Height: v.Size,
-		Grid:   v.Grid,
+		Grid:   dungeonToGridPoints(v.Grid),
 	}
 
 	return json.NewEncoder(w).Encode(resp)
+}
+
+func dungeonToGridPoints(dungeon [][]int) []gridPoint {
+	size := len(dungeon)
+	ret := make([]gridPoint, 0, size*size)
+	var directions = []direction.Direction{
+		direction.North,
+		direction.South,
+		direction.East,
+		direction.West,
+	}
+
+	min := point.Point{
+		X: 0,
+		Y: 0,
+	}
+
+	max := point.Point{
+		X: size - 1,
+		Y: size - 1,
+	}
+
+	for y := 0; y < size; y++ {
+		for x := 0; x < size; x++ {
+			p := dungeon[y][x]
+			if p == 0 {
+				continue
+			}
+
+			gp := point.Point{
+				X: x,
+				Y: y,
+			}
+
+			next := make([]point.Point, 0, 4)
+			for _, d := range directions {
+				pn := gp.AddDirection(d)
+
+				if pn.Inside(min, max) && dungeon[pn.Y][pn.X] == 1 {
+					next = append(next, pn)
+				}
+			}
+
+			ret = append(ret, gridPoint{
+				P: gp,
+				N: next,
+			})
+		}
+	}
+	return ret
 }
